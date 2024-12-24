f31971a49bf6d48aece496f8b25a053b6feb0d4d
Karthik ck <karthikck215@gmail.com>
2024-12-24 11:55:51 +0530

    samsung-678-Hello World! we using jekins and maven

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -5,6 +5,6 @@ package demo;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! we using jekins and maven");
     }
 }
